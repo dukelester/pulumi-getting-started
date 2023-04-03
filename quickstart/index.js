8f21058a841193b966c8592916d78931ec2da9d9
dukelester <dukelester4@gmail.com>
2023-04-04 00:50:08 +0300

    Added the files and upload to the bucket

--- quickstart/index.js
+++ quickstart/index.js
@@ -6,6 +6,10 @@ const bucket = new gcp.storage.Bucket('my-bucket', {
   location: 'US',
 });
 
+const bucketObject = new gcp.storage.BucketObject('index.html', {
+  bucket: bucket.name,
+  source: new pulumi.asset.FileAsset('index.html'),
+});
 // Export the DNS name of the bucket
 exports.bucketName = bucket.url;
 exports.bucketLocation = bucket.location;
