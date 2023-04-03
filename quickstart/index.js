5ef9b31579d0fcfd2f858b303b678fc8dd6c521c
dukelester <dukelester4@gmail.com>
2023-04-04 00:02:32 +0300

    Started the javacript simple pulumi project

---
+++ quickstart/index.js
@@ -0,0 +1,13 @@
+const pulumi = require('@pulumi/pulumi');
+const gcp = require('@pulumi/gcp');
+
+// Create a GCP resource (Storage Bucket)
+const bucket = new gcp.storage.Bucket('my-bucket', {
+  location: 'US',
+});
+
+// Export the DNS name of the bucket
+exports.bucketName = bucket.url;
+exports.bucketLocation = bucket.location;
+exports.name = bucket.name;
+exports.ptoject = bucket.project;
