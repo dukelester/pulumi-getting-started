f66550f43d4018f7d5b660209087b037a353de60
dukelester <dukelester4@gmail.com>
2023-04-04 12:47:16 +0300

    Pulling the images

--- MERN-stack-app/index.js
+++ MERN-stack-app/index.js
@@ -1,2 +1,25 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
+const docker = require('@pulumi/docker');
+
+
+const stack = pulumi.getStack();
+
+// Pull the backend image
+const backendImageName = 'backend';
+const backend = new docker.RemoteImage(`${backendImageName}Image`, {
+    name: 'dukelester/backend:latest',
+});
+
+// Pull the frontend image
+const frontendImageName = 'frontend';
+const frontend = new docker.RemoteImage(`${frontendImageName}Image`, {
+    name: 'dukelester/frontend:latest',
+});
+
+// Pull the database image
+
+const databaseImage = 'database';
+const database = new docker.RemoteImage(`${databaseImage}Image`, {
+    name: 'dukelester/database:latest',
+});
